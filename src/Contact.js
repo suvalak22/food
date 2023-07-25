c94444aa1f60c25e54d0bc2ea64a5986e69cc1dc
suvalak22 <suvalak.na.65@ubu.ac.th>
2023-07-25 12:52:37 +0700

    recat-create app

---
+++ src/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Layout } from "./App";
+
+function Contact() {
+  return (
+    <>
+      <Layout />
+      <h3>Ubon Ratchathani University</h3>
+      <div>
+        <img src="IMG_5859.jpg" width="300" height="380" border-radius="20%" />
+        <br />
+        นางสาวสุวลักษณ์ นามแสง
+        <br />
+        65114340899
+        <br />
+        คณะวิทยาศาสตร์
+        <br />
+        สาขาเทคโนโลยีสารสนเทศและการสื่อสาร
+      </div>
+    </>
+  );
+}
+
+export default Contact;
